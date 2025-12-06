5cc9ad86b9316ddfa8d3eec895423f57128a01b2
canduki21 <cacndela@gmail.com>
2025-12-06 14:42:07 -0500

    Update mainc.py

--- mainc.py
+++ mainc.py
@@ -2,14 +2,13 @@ from machine import Pin, I2C
 import time
 from as7343 import AS7343
 
-# GP18 = SDA, GP19 = SCL on Pico
 i2c = I2C(1, scl=Pin(19), sda=Pin(18), freq=400000)
 
-print("I2C scan:", i2c.scan())
+print("Scan:", i2c.scan())
 
 sensor = AS7343(i2c)
 
 while True:
-    data = sensor.read_channels()
-    print("Spectral Channels:", data)
+    vals = sensor.read_channels()
+    print(vals)
     time.sleep(1)
